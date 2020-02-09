75f17481838312fd7723cce161553f210a043812
John Bedalov <john.bedalov@gmail.com>
2020-02-09 16:40:43 -0600

    Refactored request to be immutable

--- src/main/java/com/bedalov/sample/web/TransformationRequest.java
+++ src/main/java/com/bedalov/sample/web/TransformationRequest.java
@@ -1,6 +1,8 @@
 package com.bedalov.sample.web;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import javax.validation.constraints.NotNull;
 
 public class TransformationRequest {
@@ -8,11 +10,12 @@ public class TransformationRequest {
     @NotNull(message = "Null is not a valid input.")
     private Integer input;
 
-    public Integer getInput() {
-        return input;
+    @JsonCreator
+    public TransformationRequest(Integer input) {
+        this.input = input;
     }
 
-    public void setInput(Integer input) {
-        this.input = input;
+    public Integer getInput() {
+        return input;
     }
 }
